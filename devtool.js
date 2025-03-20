cfb9438b0fb9caabbba7a48f4a73e2a490bd96b4
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2025-03-20 16:44:57 -0400

    bug fixing

--- devtool.js
+++ devtool.js
@@ -173,17 +173,20 @@ addEventListener("DOMContentLoaded", (event) => {
             console.error(error);
         }
     }
+    consoleInput.addEventListener("keyup", () => {
+        consoleInput.style.height = calcHeight(consoleInput.value) + "px";
+    });
 
     consoleInput.addEventListener("keypress", function (event) {
         if (event.key === "Enter" && !shiftKey) {
             executeCommand();
         }
-        consoleInput.style.height = calcHeight(consoleInput.value) + "px";
     });
     function calcHeight(value) {
         let numberOfLineBreaks = (value.match(/\n/g) || []).length;
-        //min-height + lines x line-height + padding + border
-        return 20 + numberOfLineBreaks * 20 + 12 + 2;
+        // min-height + lines x line-height + padding + border
+        let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+        return newHeight;
     }
 
     // Override console log methods
