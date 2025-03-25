adbbc42ed6ce770dcc90d5953af4b99f8b746735
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2025-03-25 07:55:40 -0400

    test

--- devtool.js
+++ devtool.js
@@ -327,6 +327,7 @@ addEventListener("DOMContentLoaded", (event) => {
             lastB = event.target.style.border
             lastEv = event.target
             event.target.style.border = "1px solid blue !important"
+            viewer.innerHTML = 'salut'
             viewer.innerHTML = event.target.outerHTML.replaceAll('<','<p class="element">&lt;').replaceAll('>','&gt;')
             console.log(event.clientY,event.clientX,viewer)
         };
