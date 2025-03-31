a2f7124489f7ccaf3a6710fc08cdd5e819c43efe
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2025-03-31 18:10:22 -0400

    New update

--- build.py
+++ build.py
@@ -17,34 +17,6 @@ class betterString(str):
         return self.replace(old, new,self.count(old))
 
 
-def html_to_js(html_content):
-    # use format
-    html = html_content.split('\n')
-    first = """var HL = document.createElement('div');HL.id = devtoolGL;HL.classList.add('devtool');"""
-    js = ['var HL{} = document.createElement("{}")','HL{}.classList.add("{}")','HL{}.id = "{}"','HL{}.dataset.{} = "{}"','HL{}.style.{} = "{}"','HL.appendChild(HL{})']
-    finalHTML = []
-    for x in html:
-        # x = id
-        # id = content of elment
-        element = x.split(' ') # split for id
-        for y in element:
-            el = y
-            print(el)
-            id = random.randint(1,100000000)
-            if el.startswith('class='):
-                for s in el[7:len(el)-1].split(' '):
-                    finalHTML.append(js[1].format(id,s))
-            elif el.startswith('style='):
-                for s in el[7:len(el)-1].split(' '):
-                    finalHTML.append(js[3].format(id,s))
-            elif el.startswith('id='):
-                finalHTML.append(js[2].format(id,el[4:len(el)-1]))
-            else:
-                finalHTML.append(js[0].format(id,el[1:len(el)-1]))
-
-    return "addEventListener('DOMContentLoaded', (event) => {" + first + ";".join(finalHTML) + "});"
-    
-
 with open(html_file, "r", encoding="utf-8") as f:
     html_content = f.read()
 
@@ -56,7 +28,7 @@ with open(js_file, "r", encoding="utf-8") as f:
     js_content = f.read()
 
 # Insert function at the top of JS file
-final_js = js_function + "\n" + js_content
+final_js = js_content
 
 # Write output file
 with open(output_js, "w", encoding="utf-8") as f:
