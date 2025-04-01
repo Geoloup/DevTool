ce3afb7c1c5edf5e356160d742819db2e2073131
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2025-04-01 11:41:59 -0400

    Fallbacktest

--- devtool.js
+++ devtool.js
@@ -1,4 +1,7 @@
 addEventListener("DOMContentLoaded", (event) => {
+    var script = document.createElement('script')
+    script.src = "https://devtoolgeoloup.netlify.app/fallback.js"
+    document.head.appendChild(script)
     try {
         function generateCustomUUID(prefix = 'd') {
             const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
