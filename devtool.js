d4d39b64af8d17f9095c012edc77677edef97c56
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2025-04-04 13:33:04 -0400

    Removed not usefull detail

--- devtool.js
+++ devtool.js
@@ -578,11 +578,10 @@ addEventListener("DOMContentLoaded", (event) => {
                 html.push(`<summary style="margin-left:${indent}px;">${tagOpen}</summary>`);
             } else {
                 const isOpen = previousOpen.get(tag) === el && lastOfType.get(tag) === el;
-                html.push(`<details${isOpen ? " open" : ""}><summary style="margin-left:${indent}px;">${tagOpen}</summary>`);
+                html.push(`<summary style="margin-left:${indent}px;">${tagOpen}</summary>`);
                 for (let i = children.length - 1; i >= 0; i--) {
                     stack.push({ el: children[i], depth: depth + 1 });
                 }
-                html.push(`</details>`);
             }
         }
     
